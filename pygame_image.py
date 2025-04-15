54ef3a6caf2f3b2d31e2b016c0ee462130ec558a
c0c24040d1 <c0c24040d1@edu.teu.ac.jp>
2025-04-15 16:08:01 +0900

    ex7

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_flip = pg.transform.flip(bg_img,True,False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
     kk_img = pg.transform.rotozoom(kk_img,10,1.0)
@@ -19,7 +20,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         x=tmr
-        screen.blit(bg_img, [x, 0])
+        if x==3200:
+            tmr=0
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_flip, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kk_img, [300,200])
         pg.display.update()
         tmr += 1        
